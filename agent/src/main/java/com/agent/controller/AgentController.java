7eed23fed56ff96eef0103d2a9e9a3dcc8fb0828
proms <h_proms@naver.com>
2017-10-30 17:01:38 +0900

    건주 연결 확인

--- agent/src/main/java/com/agent/controller/AgentController.java
+++ agent/src/main/java/com/agent/controller/AgentController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Brian H Sun
- * @see 연결확인
+ * @see 연결확인   건주 연결 확인
  */
 @RestController
 public class AgentController {
